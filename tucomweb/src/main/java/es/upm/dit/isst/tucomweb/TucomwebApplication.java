2d0bcfc9e86cde8c36fd29b99bbad8863b851b7d
lgcayetano <125416214+lgcayetano@users.noreply.github.com>
2023-03-31 17:23:05 +0200

    Creacion proyecto SB tucomweb

---
+++ tucomweb/src/main/java/es/upm/dit/isst/tucomweb/TucomwebApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.tucomweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TucomwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TucomwebApplication.class, args);
+	}
+
+}
---
+++ tucomweb/src/test/java/es/upm/dit/isst/tucomweb/TucomwebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.tucomweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TucomwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
