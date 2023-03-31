a745893c708cb223ca84b14e276d8a2fc5a7fbf8
lgcayetano <125416214+lgcayetano@users.noreply.github.com>
2023-03-31 17:01:44 +0200

    Creación proyecto SB tucomapi

---
+++ tucomapi/src/main/java/es/upm/dit/isst/tucomapi/TucomapiApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.tucomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TucomapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TucomapiApplication.class, args);
+	}
+
+}
---
+++ tucomapi/src/test/java/es/upm/dit/isst/tucomapi/TucomapiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.tucomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TucomapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
