19c9e989590fcb9f9fca3838308212a0da213c73
lgcayetano <125416214+lgcayetano@users.noreply.github.com>
2023-04-04 23:14:40 +0200

    Actualización gitignore para no sincronizar node

--- tucomweb/frontend/src/ComunidadList.js
+++ tucomweb/frontend/src/ComunidadList.js
@@ -7,7 +7,7 @@ class ComunidadList extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {comunidades: []}; 
+        this.state = {comunidades: []};
     }
 
     componentDidMount() {
