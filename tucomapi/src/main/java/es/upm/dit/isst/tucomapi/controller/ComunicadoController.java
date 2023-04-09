7b69dddce3fa0d338d7e6c7988ecdea1a7876ae0
Enrique García <egarciadealfonso@gmail.com>
2023-04-09 14:02:04 +0200

    nuevo comunicado

--- tucomapi/src/main/java/es/upm/dit/isst/tucomapi/controller/ComunicadoController.java
+++ tucomapi/src/main/java/es/upm/dit/isst/tucomapi/controller/ComunicadoController.java
@@ -9,8 +9,12 @@ import java.util.List;
 import es.upm.dit.isst.tucomapi.model.Usuario;
 import es.upm.dit.isst.tucomapi.repository.UsuarioRepository;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.upm.dit.isst.tucomapi.model.Comunicado;
@@ -32,7 +36,7 @@ public class ComunicadoController {
       this.usuarioRepository = t2;
     }
 
-    @GetMapping("/comunicados")
+    @GetMapping("/comunicados") /*muestra los comunicados  */
     List<Comunicado> readAll(Principal principal) {
 
       Usuario Usuario = usuarioRepository.findByEmail(principal.getName()).orElse(null);
@@ -40,9 +44,37 @@ public class ComunicadoController {
 
       if (Usuario!=null)
         idComunidad = Usuario.getIdcomunidad();
+
       return (List<Comunicado>) comunicadoRepository.findAllByIdComunidad(idComunidad);
 
-      /*devolver comunicados */
+    }
+
+    
+    /*public ResponseEntity<Comunicado> nuevoComunicado(@RequestBody Comunicado comunicado) {
+
+      comunicadoRepository.save(comunicado);
+
+      return ResponseEntity.ok().build();*/
+      @PostMapping("/comunicados/nuevo")
+      public Comunicado registerComunicado() {
+
+      
+          
+      Comunicado newComunicado = new Comunicado();
+        
+        newComunicado.setId(89);
+        newComunicado.setTitulo("titulo de prueba");
+        newComunicado.setMensaje("hola que tal esto es el mensaje");
+        newComunicado.setFecha(null);
+        newComunicado.setIdusuario(1);
+        newComunicado.setIdComunidad(2);
+        
+
+          
+        
+      
+
+      return comunicadoRepository.save(newComunicado);
 
     }
 }
