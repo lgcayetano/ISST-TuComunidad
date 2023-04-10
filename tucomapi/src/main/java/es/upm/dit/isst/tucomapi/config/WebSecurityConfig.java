e7c607281f52a94c39314e2a494ba8e6fdfdd101
lgcayetano <125416214+lgcayetano@users.noreply.github.com>
2023-04-10 02:52:33 +0200

    Deshabilitar redirigir al login en la api

--- tucomapi/src/main/java/es/upm/dit/isst/tucomapi/config/WebSecurityConfig.java
+++ tucomapi/src/main/java/es/upm/dit/isst/tucomapi/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -34,6 +35,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+        .exceptionHandling()
+        .authenticationEntryPoint(new Http403ForbiddenEntryPoint())
+        .and()
         .cors().and()
 		.csrf().disable()
         .authorizeRequests()
