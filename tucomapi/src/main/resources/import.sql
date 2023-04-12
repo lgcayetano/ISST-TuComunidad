435d82ef3d2b1144516bcdceb2f9380a14d2d043
lgcayetano <125416214+lgcayetano@users.noreply.github.com>
2023-04-12 23:49:30 +0200

    Import SQL comunidad vecinos presentacion

--- tucomapi/src/main/resources/import.sql
+++ tucomapi/src/main/resources/import.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO Comunidad (id, nombre, codigopresidente, codigovecino, estado) VALUES (1, 'Comunidad 1','PresiCom1','VeciCom1',1),(2, 'Comunidad 2','PresiCom2','VeciCom2',1);
+INSERT INTO Comunidad (id, nombre, codigopresidente, codigovecino, estado) VALUES (1, 'Comunidad 1','PresiCom1','VeciCom1',1),(2, 'Comunidad 2','PresiCom2','VeciCom2',1),(3, 'Com. de Prop. C/Desengaño, 21','2a3b4637-a527-4dfa-ae99-3f9541278be7','61a487f1-b82a-4889-8537-fb9376aa8c13',1);
 
 INSERT INTO Usuario (id,nombre,email,contrasena,nivel,estado,idcomunidad,permisos) VALUES (1,'Presidente 1','presi1@email.com','presi1',1,1,1,1),(2,'Vecino 1','veci1@email.com','veci1',2,1,1,1),(3,'Presidente 2','presi2@email.com','presi2',1,1,2,1),(4,'Vecino 2','veci2@email.com','veci2',2,1,2,1);
 
