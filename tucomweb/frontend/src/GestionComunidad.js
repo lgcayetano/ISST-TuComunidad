f38c8b9a26254fa6c743e84e9990e8209886ff1b
Victor Pilot <vic.pilot@hotmail.com>
2023-04-29 01:10:04 +0200

    gestionComunidad 0.2

--- tucomweb/frontend/src/GestionComunidad.js
+++ tucomweb/frontend/src/GestionComunidad.js
@@ -30,14 +30,14 @@ export default function GestionComunidad () {
         <div>
             <Header />
             
-            <div className="comunicados" >
+            <div className="comunicados" style={{marginBottom:"20px"}}>
 
                 <Form /*onSubmit={handleSubmit}*/>
                     <FormGroup className="cambioNombre">
                         <span style={{display:"inline"}}><Input className="innnput" type="text" placeholder={state.nombre}
-                             value={state.nombre}  onChange={handleChange}/>
+                             value={state.nombre}  onChange={handleChange}  />
                        
-                        <Button type="submit" >Cambiar nombre comunidad</Button>
+                        <Button className='buton' type="submit" >Cambiar nombre comunidad</Button>
                         </span>
                         
                     </FormGroup>
@@ -45,45 +45,53 @@ export default function GestionComunidad () {
                     <FormGroup className="cambioNombre">
                         <Input className="innnput" type="text" placeholder={state.comunidad}
                              value={state.comunidad}  onChange={handleChange}/>
-                        <Button type="submit">Cambiar código comunidad</Button>
+                        <Button className='buton' type="submit">Cambiar código comunidad</Button>
                     </FormGroup> 
                 </Form>
+            </div>
 
-                <div className="comunicados" >
+            <div className="comunicados" >
+                <h3 style={{color:"rgb(164, 40, 40)"}}>LISTA DE USUARIOS</h3>
+                    {/*
+                    la parte de abajo se elimina en cuanto esté lo de state.usuarios y eso
+                    */}
+
                     
-                     <div>
+                    <div >
+                        <div style={{float:"left", marginRight:"20px", width:"20%"}}>
                             <b>nombre y apellidos</b> 
                             <p>email@email.com</p> 
-                            <Button> Ceder presidencia </Button>
-                            <Button> Quitar/dar permisos </Button>
-                            <Button> X </Button>
+                        </div>
+                        <Button className="buton"> Ceder presidencia </Button>
+                        <Button className='buton'> Quitar/dar permisos </Button>
+                        <Button className='buton'> X </Button>
+                    </div>
+                    <div style={{marginTop:"20px"}}>
+                        <div style={{float:"left", marginRight:"20px", width:"20%"}}>
+                            <b>nombre y apellidos Muchos</b> 
+                            <p>email@email.com</p> 
+                        </div>
+                        <Button className="buton"> Ceder presidencia </Button>
+                        <Button className='buton'> Quitar/dar permisos </Button>
+                        <Button className='buton'> X </Button>
                     </div>
                     
 
                 {
                     state.usuarios && state.usuarios.map(usuario => (
-                        <div key={usuario.id} >
-                            <b>{usuario.nombre}</b> 
-                            <p>{usuario.email}</p> 
-                            <Button> Ceder presidencia </Button>
-                            <Button> Quitar/dar permisos </Button>
-                            <Button> X </Button>
+                        <div key={usuario.id} style={{marginTop:"20px"}}>
+                            <div style={{float:"left", marginRight:"20px", width:"20%"}}>
+                                <b>{usuario.nombre}</b> 
+                                <p>{usuario.email}</p> 
+                            </div>
+                           <Button className="buton"> Ceder presidencia </Button>
+                           <Button className="buton"> Quitar/dar permisos </Button>
+                           <Button className="buton"> X </Button>
 
-                        </div>
-                    ))
-                }
-                </div>
-
-                
-
-
-
-
-                
-
-
-
-            </div>
+                       </div>
+                   ))
+               }
+               </div>
 
             <div className="footer" >
                 <b>TuComunidad 2023</b>
