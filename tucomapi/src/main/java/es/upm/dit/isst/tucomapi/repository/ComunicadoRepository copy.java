f6e8721c64baa213864479616f6ed9f594fae0a0
CarmenNovillo <cramen2000@gmail.com>
2023-05-02 14:13:34 +0100

    sugerencias commit

---
+++ tucomapi/src/main/java/es/upm/dit/isst/tucomapi/repository/ComunicadoRepository copy.java
@@ -0,0 +1,12 @@
+package es.upm.dit.isst.tucomapi.repository;
+
+import es.upm.dit.isst.tucomapi.model.Sugerencia;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface SugerenciaRepository extends CrudRepository<Sugerencia, Integer> {
+}
