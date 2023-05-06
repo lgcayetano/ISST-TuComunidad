022c7a60e074feca2e0a23d4199c2a3fd5cf76ea
Enrique García <egarciadealfonso@gmail.com>
2023-05-06 18:42:15 +0200

    busqueda usuario

--- tucomapi/src/main/java/es/upm/dit/isst/tucomapi/repository/UsuarioRepository.java
+++ tucomapi/src/main/java/es/upm/dit/isst/tucomapi/repository/UsuarioRepository.java
@@ -15,7 +15,7 @@ public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
     @Query(value = "SELECT * FROM Usuario WHERE email=?1", nativeQuery = true)
     Optional<Usuario> findByEmail(String email);
 
-    @Query(value = "SELECT usuario.id AS id, usuario.nombre AS nombre, usuario.email AS email, usuario.permisos AS permisos FROM Usuario WHERE idcomunidad=?1 ORDER BY id DESC", nativeQuery = true)
+    @Query(value = "SELECT usuario.id AS id, usuario.nombre AS nombre, usuario.email AS email, usuario.permisos AS permisos FROM Usuario WHERE idcomunidad=?1 AND estado=1 ORDER BY id DESC", nativeQuery = true)
     /*Iterable<Usuario> findAllUsersByIdComunidad(int idcomunidad);*/
     List<UsuarioDTO> findAllByIdComunidad(int idcomunidad);
 
